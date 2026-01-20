ccdd9c6d18028bd8874a5048d78c0ce944f5ff1c
BigSky-1208 <sora.yamashita@neo-career.co.jp>
2026-01-20 18:23:44 +0900

    Update utils.js

--- js/components/modal/utils.js
+++ js/components/modal/utils.js
@@ -18,13 +18,19 @@ export function showHelpModal(pageKey) {
     const contentEl = document.getElementById("help-modal-content");
     if (!helpModal || !titleEl || !contentEl) return;
 
+const [iframeKey, setIframeKey] = useState(0);
+    const reloadDoc = () => {
+  setIframeKey(k => k + 1)
+}
+
 const helpContents = {
     client: {
         title: "従業員画面（業務記録）ヘルプ",
         content: `
             <div class="w-full h-[65vh] bg-white rounded border border-gray-200 overflow-hidden">
                 <iframe 
-                    src="https://docs.google.com/document/d/e/2PACX-1vTLexuUJP55J8FKQG8s4rjxLS1r7rJcKgnIPapyYN4t8kKnYqArMnCA-ymkWaX_Wqf7pR8vkjmbzz_Q/pub?embedded=true&t=${Date.now()}" 
+                key={iframeKey}
+                    src="https://docs.google.com/document/d/e/2PACX-1vTLexuUJP55J8FKQG8s4rjxLS1r7rJcKgnIPapyYN4t8kKnYqArMnCA-ymkWaX_Wqf7pR8vkjmbzz_Q/pub?embedded=true" 
                     class="w-full h-full block" 
                     frameborder="0">
                 </iframe>
